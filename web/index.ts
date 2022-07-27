6714980f6e652230e7089be65279e3999a6770bd
Ben Howarth <benhowarth6@gmail.com>
2022-07-27 15:50:26 +0100

    Update index.ts

--- web/index.ts
+++ web/index.ts
@@ -145,16 +145,16 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Vercel', value: 'https://db.ipatchrepairs.co.uk/logo-solo.svg' },
+    { text: 'Next.js', value: 'https://db.ipatchrepairs.co.uk/logo-solo.svg' },
+    { text: 'Hyper', value: 'https://db.ipatchrepairs.co.uk/logo-solo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Vercel', value: 'https://db.ipatchrepairs.co.uk/logo-solo.svgg' },
+    { text: 'Next.js', value: 'https://db.ipatchrepairs.co.uk/logo-solo.svg' },
+    { text: 'Hyper', value: 'https://db.ipatchrepairs.co.uk/logo-solo.svg' },
 ];
 
 
